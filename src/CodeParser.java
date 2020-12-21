724a3a36ce2b8a50ca28af9e611b78065bc40ff5
jun <1010479088@qq.com>
2020-12-21 18:37:17 +0800

    1.逻辑添加

--- src/CodeParser.java
+++ src/CodeParser.java
@@ -1,27 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CodeParser {
-    private final String TEST0 = "打码：8848;序列:4打码时间：00:00:03.9892996";
-    private final String TEST = "打码：8848;序列:4打码时间：00:00:03.9892996\n打码：1854;序列:9打码时间：00:00:01.9104948\n打码：1535;序列:11打码时间：00:00:02.1681648";
-    private final Pattern pattern = Pattern.compile("打码：(\\d+);序列:(\\d+)打码时间：00:00:03.9892996");
+    private static final int MAX = 2;
+    private final Pattern pattern = Pattern.compile("打码：(\\d+);序列:(\\d+)打码时间：00:00:(\\d+).(\\d+)");
     private String path;
 
     public CodeParser(String path) {
         this.path = path;
-        String parsedLine = parse(TEST0);
-        LogUtil.log(parsedLine);
+        start();
+    }
+
+    private void start() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String parsedResult = parse(line);
+                if (!TextUtils.isEmpty(parsedResult)) {
+                    LogUtil.log(parsedResult);
+                }
+            }
+        } catch (Exception ex) {
+            LogUtil.log(ex.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (Exception ex) {
+                LogUtil.log(ex.getMessage());
+            }
+        }
     }
 
     private String parse(String line) {
         if (!TextUtils.isEmpty(line)) {
             Matcher matcher = pattern.matcher(line);
-            boolean find = matcher.find();
-            if (find) {
+            StringBuilder builder = new StringBuilder();
+            while (matcher.find()) {
                 String result = matcher.group(1);
                 String index = matcher.group(2);
-                return result + "|" + index;
+                String timeSecond = matcher.group(3);
+                String timeRemain = matcher.group(4);
+                if (!TextUtils.isEmpty(timeRemain) && timeRemain.length() > MAX) {
+                    timeRemain = timeRemain.substring(0, MAX);
+                }
+                builder.append(String.format("序号:%s, 打码:%s, 时间:%s.%s", index, result, timeSecond, timeRemain));
             }
+            return builder.toString();
         }
         return "";
     }
