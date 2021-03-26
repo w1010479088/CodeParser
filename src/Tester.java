0abad871d4f8d782e004ef85547bdb3be7f6e58e
jun <1010479088@qq.com>
2021-03-26 21:54:52 +0800

    1.Pattern重构

--- src/Tester.java
+++ src/Tester.java
@@ -4,14 +4,11 @@ import utils.BasePage;
 import java.util.regex.Pattern;
 
 public class Tester {
-    private static final String PATTERN = "2020/12/19 11:29:28|    打码：8848;序列:4打码时间：00:00:03.9892996";
-    private static final Pattern pattern = Pattern.compile("11:29:(\\d+)\\|    打码：(\\d+);序列:(\\d+)打码时间：00:00:(\\d+).(\\d+)");
-
     public static void main(String[] args) {
         new BasePage() {
             @Override
             protected void action(String path) {
-                new CodeParser(path, pattern, new CodeParser.OnParseListener() {
+                new CodeParser(path, CodeType.OLD.pattern(), new CodeParser.OnParseListener() {
                     @Override
                     public void onStart() {
                         log("解析开始!");
@@ -35,5 +32,22 @@ public class Tester {
                 return "Code解析";
             }
         }.showUI();
+    }
+
+    public enum CodeType {
+        OLD("2020/12/19 11:29:28|    打码：8848;序列:4打码时间：00:00:03.9892996", Pattern.compile("11:29:(\\d+)\\|    打码：(\\d+);序列:(\\d+)打码时间：00:00:(\\d+).(\\d+)")),
+        NEW("", Pattern.compile(""));
+
+        private String demo;
+        private Pattern pattern;
+
+        public Pattern pattern() {
+            return pattern;
+        }
+
+        CodeType(String demo, Pattern pattern) {
+            this.demo = demo;
+            this.pattern = pattern;
+        }
     }
 }
